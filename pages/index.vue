34a18b8bdc15b5eb2a407ea784a3be50a3e93c33
Philippe Ducasse <ducassephi@hotmail.fr>
2025-02-18 20:59:22 +0100

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>philocircus Home page</div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss" scoped></style>
---
+++ pages/projects/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>Projects page</div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
