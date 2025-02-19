853b91f5790308b060c16b5b920dcb5f6d50a4ba
Philippe Ducasse <ducassephi@hotmail.fr>
2025-02-19 18:40:15 +0100

    added navbar

---
+++ layouts/default.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <Navbar />
+    <slot />
+  </div>
+</template>
+
+<script setup lang="ts">
+import Navbar from "~/components/Navbar.vue";
+</script>
+
+<style scoped></style>
