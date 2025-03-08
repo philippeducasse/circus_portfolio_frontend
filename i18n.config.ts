859efeca70324ed895356b04bf30a1f14e771275
Philippe Ducasse <ducassephi@hotmail.fr>
2025-03-08 20:10:15 +0100

    Added translations

--- i18n.config.ts
+++ i18n.config.ts
@@ -5,22 +5,75 @@ export default defineI18nConfig(() => ({
     en: {
       welcome: "Welcome",
       projects_header:
-        "Philippe's performances blend circus, theatre and dance to connect with audiences over a variety of topics.",
-      lulu: 'Lulu is an 45 minute contemporary circus piece created and performed by Philippe. It tells the story of a troubled individual desperately searching for the solution to all of his problems - "Lulu". The varied piece includes mime, object manipulation, acrobatics, and dance - as well as strong use of dark humour.',
-      abb: "Ah Bah Bravo (french for “well done then!”) Is an interactive, funny and light-hearted contemporary circus show, ideal for family audiences. Created and interpreted by Philippe Ducasse, the length of the piece is flexible between 20 and 45 minuntes, and is designed to be presented on the street and open air stages.",
+        "Philippe's performances blend circus, theatre, and dance to connect with audiences over a variety of topics.",
+      lulu: "Lulu is a 45-minute contemporary circus piece created and performed by Philippe. It tells the story of a troubled individual desperately searching for the solution to all of his problems - 'Lulu'. The varied piece includes mime, object manipulation, acrobatics, and dance - as well as strong use of dark humour.",
+      abb: "Ah Bah Bravo (French for 'well done then!') is an interactive, funny, and light-hearted contemporary circus show, ideal for family audiences. Created and performed by Philippe Ducasse, the piece can be adapted between 20 and 45 minutes and is designed for street and open-air performances.",
       stone:
-        'Philippe has created several shorter performances, many between 5-15 minutes. These include "Stone", which explores physicality, dramaturgy and dance through manipulation of the contact staff. Philippe also regularly performs a wide range of mime, clown, and fire-spinning pieces.',
+        'Philippe has created several shorter performances, many between 5-15 minutes. These include "Stone", which explores physicality, dramaturgy, and dance through manipulation of the contact staff. Philippe also regularly performs a wide range of mime, clown, and fire-spinning pieces.',
       about:
-        "Philippe Ducasse is a contemporary circus artist, specialising in contact staff and contact ball juggling. He also works across disciplines and is trained in a variety of other forms including clowning, handstands, dance, acrobatics, fire-spinning and mime. Growing up between Paris and Milan, Philippe was captivated by painting and music from an early age, and as a teenager quickly fell in love with dance and juggling. After obtaining a bachelor’s degree in psychology from the University of Exeter, he attended a martial arts school in China for nine months. He went on to complete a three year circus programme at Die Etage School for Performance and Visual Arts in Berlin, graduating with a focus on object manipulation. Since then Philippe has performed around the world in various street, music, and juggling festivals including Usedome Kleinkunst Festival, International Storytelling Festival, Aurillac Festival, Fusion Festival, Brianza Juggling Convention and others. Philippe is based in Berlin, where he also performs and teaches.",
+        "Philippe Ducasse is a contemporary circus artist, specializing in contact staff and contact ball juggling. He also works across disciplines and is trained in a variety of other forms including clowning, handstands, dance, acrobatics, fire-spinning, and mime. Growing up between Paris and Milan, Philippe was captivated by painting and music from an early age, and as a teenager quickly fell in love with dance and juggling. After obtaining a bachelor’s degree in psychology from the University of Exeter, he attended a martial arts school in China for nine months. He went on to complete a three-year circus program at Die Etage School for Performance and Visual Arts in Berlin, graduating with a focus on object manipulation. Since then, Philippe has performed around the world in various street, music, and juggling festivals including Usedom Kleinkunst Festival, International Storytelling Festival, Aurillac Festival, Fusion Festival, Brianza Juggling Convention, and others. Philippe is based in Berlin, where he also performs and teaches.",
+      dates_past: "Past Performances",
+      "2024": [
+        "22-24th February: Performance for Arte Documentary 'Bugatti Step', Leipzig, Germany",
+        "18-24th March: Lulu & Ah Bah Bravo! at Kavc Festival, Ljubljana, Slovenia",
+        "28-30th March: Moha at Zirkus Mond, Berlin, Germany",
+        "9-10th May: Lulu & Ah Bah Bravo! at KLP Festival, Mützingen, Germany",
+        "17-19th May: Ah Bah Bravo! at KLP Festival, Güstritz, Germany",
+        "15-16th June: Lulu & Ah Bah Bravo! at Gauklerfest, Germany",
+        "23rd July: I become a dad! of TWINS!!",
+        "17th August: Lulu at Gute Laune Festival, Halle, Germany",
+        "2nd of November: New WIP piece at Neue Bühne Friedrichshain, Berlin, Germany",
+      ],
+      "2023": [
+        "February 1-5th: Lulu & Ah Bah Bravo! at Kavc Festival, Ljubljana, Slovenia",
+        "17-19th March: Lulu at Theaterforum Kreuzberg, Berlin, DE",
+        "30th April: Ah Bah Bravo! At Bad Nenndorf, DE",
+        "18-20th May: Ah Bah Bravo! At KLP Festival, DE",
+        "23-25th May: Lulu at Brighton Fringe Festival, UK",
+        "26-28th May: Ah Bah Bravo! At KLP Festival, DE",
+        "29th May: Lulu at Freiburg Jongliere Convention, DE",
+        "2-3rd June: Moha at Zirkus Mond, Berlin, DE",
+        "10th June: Ah Bah Bravo! at Jahninselfest, Augsburg, DE",
+        "9th July: Ah Bah Bravo! at Nikolaiveirtel, Berlin, DE",
+      ],
+      "2022": [
+        "9-10 October - LULU at Villa Kuriosum, Berlin, Germany",
+        "27 August - MOHA at Phoenix Fire Convention, Germany",
+        "26 August - LULU at Parasola Festival, Klingelmühle, Germany (CANCELED)",
+        "17 - 20 August - LULU & AH BAH BRAVO! at Festival d´Aurillac, France",
+      ],
+      "2021": [
+        "03. December - LULU at Zirkusquartier, Zürich, Switzerland",
+        "5. August - LULU at Sommermusikfest, Wolfstein, Germany",
+      ],
     },
-
     fr: {
       welcome: "Bienvenue",
       projects_header:
-        "Les spectacles de Philippe mélangent le cirque, le théâtre et la danse pour établir un lien avec le public sur des sujets variés.",
-      lulu: "Lulu est une pièce de cirque contemporain de 45 minutes créée et interprétée par Philippe. Elle raconte l'histoire d'un individu perturbé qui cherche désespérément la solution à tous ses problèmes - \"Lulu\". Cette pièce variée comprend du mime, de la manipulation d'objets, de l'acrobatie et de la danse, ainsi qu'une forte dose d'humour noir.",
-      abb: "",
-      stone: "",
+        "Les spectacles de Philippe mêlent cirque, théâtre et danse pour établir un lien avec le public sur divers sujets.",
+      lulu: "Lulu est un spectacle de cirque contemporain de 45 minutes, créé et interprété par Philippe. Il raconte l'histoire d'un individu troublé qui cherche désespérément la solution à tous ses problèmes - 'Lulu'. Ce spectacle varié inclut du mime, de la manipulation d'objets, de l'acrobatie et de la danse, ainsi qu'une forte dose d'humour noir.",
+      abb: "Ah Bah Bravo (français pour 'eh bien bravo !') est un spectacle de cirque contemporain interactif, drôle et léger, idéal pour un public familial. Créé et interprété par Philippe Ducasse, sa durée est flexible entre 20 et 45 minutes et il est conçu pour être joué dans la rue et sur des scènes en plein air.",
+      stone:
+        'Philippe a créé plusieurs performances courtes, souvent entre 5 et 15 minutes. Parmi elles, "Stone", qui explore la physicalité, la dramaturgie et la danse à travers la manipulation du bâton de contact. Philippe se produit également régulièrement dans une large gamme de spectacles de mime, de clown et de jonglerie avec le feu.',
+      about:
+        "Philippe Ducasse est un artiste de cirque contemporain spécialisé dans la jonglerie avec bâton de contact et balle de contact. Il travaille également dans d'autres disciplines et est formé à diverses pratiques telles que le clown, les équilibres sur les mains, la danse, l'acrobatie, la jonglerie de feu et le mime.",
+      dates_past: "Performances passées",
+      "2024": [
+        "22-24 février : Performance pour le documentaire Arte 'Bugatti Step', Leipzig, Allemagne",
+        "18-24 mars : Lulu & Ah Bah Bravo! au Festival Kavc, Ljubljana, Slovénie",
+      ],
+      "2023": [
+        "1-5 février : Lulu & Ah Bah Bravo! au Festival Kavc, Ljubljana, Slovénie",
+        "17-19 mars : Lulu au Theaterforum Kreuzberg, Berlin, Allemagne",
+      ],
+      "2022": [
+        "9-10 octobre - LULU à Villa Kuriosum, Berlin, Allemagne",
+        "27 août - MOHA à Phoenix Fire Convention, Allemagne",
+      ],
+      "2021": [
+        "3 décembre - LULU à Zirkusquartier, Zürich, Suisse",
+        "5 août - LULU à Sommermusikfest, Wolfstein, Allemagne",
+      ],
     },
   },
 }));
