3dc8b9c8ef5f97a3b7855a29de9d92c194f1a11a
Philippe Ducasse <ducassephi@hotmail.fr>
2025-03-13 16:51:33 +0100

    migrating to nuxt ui 3

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,7 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   modules: ["@nuxt/ui", "@nuxtjs/i18n", "@nuxtjs/google-fonts"],
+  css: ["~/assets/css/main.css"],
   colorMode: {
     preference: "dark",
     fallback: "dark",
