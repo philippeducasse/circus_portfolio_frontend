2fbcd704855254397dbfbcd261a415514a0afb35
Philippe Ducasse <ducassephi@hotmail.fr>
2025-03-16 13:46:36 +0100

    added support content

--- pages/support/index.vue
+++ pages/support/index.vue
@@ -1,5 +1,38 @@
 <template>
-  <div class="px-5"></div>
+  <div class="px-5">
+    <div class="flex flex-col gap-6">
+      <div class="">
+        <h2>{{ $t("support_finance") }}</h2>
+        <div class="flex gap-6">
+          <div class="bg-white">
+            <img
+              src="../../public/img/support/bundes.png"
+              width="60"
+              class=""
+              alt="Beauftragt für Bundesregierung und Kunst"
+            />
+          </div>
+          <img src="../../public/img/support/nsk.jpg" width="60" class="" alt="Neustart Kultur" />
+          <img src="../../public/img/support/gvl.jpg" width="60" class="" alt="GVL" />
+        </div>
+      </div>
+
+      <div class="">
+        <h2>{{ $t("support_artistic") }}</h2>
+        <div class="flex gap-6">
+          <img src="../../public/img/support/katapult.png" width="60" class="" alt="katapult" />
+          <img src="../../public/img/support/zm.jpg" width="60" class="" alt="Zirkus Mond" />
+          <img src="../../public/img/support/vuesch.png" width="60" class="" alt="Vuesch" />
+          <img src="../../public/img/support/ufer.png" width="60" class="" alt="Ufer Studios" />
+          <img src="../../public/img/support/schatz.png" width="60" class="" alt="Schatzinsel" />
+          <img src="../../public/img/support/Noctambules.png" width="60" class="" alt="Noctambules" />
+          <img src="../../public/img/support/etage.png" width="60" class="" alt="Die Etage" />
+          <img src="../../public/img/support/crac.png" width="60" class="" alt="CRAC" />
+          <img src="../../public/img/support/acud.jpg" width="60" class="" alt="ACUD" />
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts"></script>
