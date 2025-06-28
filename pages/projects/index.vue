384fcc1ec5500d36b2b9316bd51ee595bc6b2f8a
Philippe Ducasse <ducassephi@hotmail.fr>
2025-06-28 13:53:35 +0200

    fixed video display

--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -7,7 +7,7 @@
         <h2 class="text-left mb-6 md:mb-12">Lulu</h2>
         <p class="">{{ $t("lulu") }}</p>
       </div>
-      <div class="relative w-screen lg:w-[60%] max-w-4xl aspect-video">
+      <div class="relative w-screen self-end lg:w-[60%] max-w-4xl aspect-video">
         <iframe class="absolute w-full h-full" width="560" height="315"
           src="https://www.youtube-nocookie.com/embed/mRTKk2jQAa0?si=OsjYt2B87_LdDiao&amp;controls=0"
           title="YouTube video player"
@@ -21,7 +21,7 @@
         <h2 class="text-left mb-6 md:mb-12">Ah Bah Bravo!</h2>
         <p>{{ $t("abb") }}</p>
       </div>
-      <div class="relative w-full lg:w-[60%] max-w-4xl aspect-video">
+      <div class="relative w-full self-end lg:w-[60%] max-w-4xl aspect-video">
         <iframe class="absolute inset-0 w-full h-full" width="560" height="315"
           src="https://www.youtube-nocookie.com/embed/gq4O41LFR6g?si=g6Its14Au8zlKcCw&amp;controls=0"
           title="YouTube video player"
@@ -32,12 +32,12 @@
       </div>
     </div>
 
-    <div class="flex my-24 gap-10 flex-col lg:flex-row w-full max-w-6xl">
+    <div class="flex my-24  gap-10 flex-col lg:flex-row w-full max-w-6xl">
       <div class="flex flex-col justify-evenly flex-1 p-4">
         <h2 class="text-left mb-6 md:mb-12">Stone</h2>
         <p>{{ $t("stone") }}</p>
       </div>
-      <div class="relative w-full lg:w-[60%] max-w-4xl aspect-video">
+      <div class="relative self-end w-full lg:w-[60%] max-w-4xl aspect-video">
         <iframe class="absolute inset-0 w-full h-full" width="560" height="315"
           src="https://www.youtube.com/embed/9cfrJuHebWM?si=r7n0j84CZaZhIeoV&amp;controls=0"
           title="YouTube video player"
@@ -51,7 +51,7 @@
         <h2 class="text-left mb-6 md:mb-12">Forgotten</h2>
         <p>{{ $t("forgotten") }}</p>
       </div>
-      <div class="relative w-full lg:w-[60%] max-w-4xl aspect-video">
+      <div class="relative w-full self-end lg:w-[60%] max-w-4xl aspect-video">
         <iframe class="absolute inset-0 w-full h-full" width="560" height="315"
           src="https://www.youtube.com/embed/_9mTJzKfU1s?si=cslwc5-YOwRxw53V&amp;controls=0"
           title="YouTube video player"
