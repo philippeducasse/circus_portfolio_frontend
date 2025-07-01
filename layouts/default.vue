099354a47efa920309d0c855c17d43a27b4411f1
Philippe Ducasse <ducassephi@hotmail.fr>
2025-07-01 21:15:20 +0200

    added toast

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,11 +4,21 @@
     <ContentLayout>
       <slot />
     </ContentLayout>
+    <UButton class="mx-auto mb-5" size="md" label="Click!" @click="showToast" icon="i-lucide-rocket" />
     <Footer />
   </div>
 </template>
 
 <script setup lang="ts">
+
+const toast = useToast()
+
+function showToast() {
+  toast.add({
+    title: 'Bravo!',
+  })
+}
+
 import Navbar from "~/components/layout/Navbar.vue";
 import Footer from "~/components/layout/Footer.vue";
 import ContentLayout from "~/components/layout/ContentLayout.vue";
