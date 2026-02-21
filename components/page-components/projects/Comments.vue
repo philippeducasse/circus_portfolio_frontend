0f8fbf467a86561c131802ee2e73be1e8769a8f8
philippeducasse <ducassephi@hotmail.fr>
2026-02-21 21:10:59 +0100

    feat: added comments, wip

---
+++ components/page-components/projects/Comments.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="">
+    <div class="" v-for="comment in comments">This is a comment</div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, onMounted } from "vue";
+
+const comments = ref(null);
+
+const fetchComments = async () => {
+  try {
+    const response = await fetch("http://localhost:8000");
+    comments.value = await response.json();
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+};
+
+onMounted(() => {
+  fetchComments();
+});
+
+const props = defineProps<{
+  showName: string;
+}>();
+</script>
